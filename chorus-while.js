72fe766aae171405f38007d414901645fac9468e
Nik Malhotra <nmal@me.com>
2019-01-05 05:00:02 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+    if (repeat === 5){
+        console.log("*YUH YUH DAB!*");
+    }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
